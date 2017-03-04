d3589372d38b7e74e4c9e219d54b2631e6e5d4fc
Diaeddin Motan <Diaeddin@Diaeddins-Air.fios-router.home>
2017-03-03 23:59:42 -0500

    Final Copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,70 @@
+
 var gameWords = ['tiger', 'flower', 'apple', 'potatoes'];
 var wins = 0;
 var guessesRemaining = 12;
 var numberOfGuesses = 12;
 var letterGuessed;
 var lettersAlreadyGuessed = [];
-var currentWord="";
-var currentWordLetterIndex;
-var numberOfcurrentWordLetters;
+var currentWord = "";
+// var currentWordLetterIndex;
+// var numberOfcurrentWordLetters;
 var lettersGuessedAndCorrect = [];
 var currentWordDashes = [];
 
 document.onkeyup = function(event) {
-	resetTheGame();
+    var sound = new Audio("assets/music/bensound-ukulele.mp3");
+    sound.play();
+    resetTheGame();
     // document.querySelector("#getStarted").innerHTML = "Game Started";
     // document.querySelector("#numberOfGuesses").innerHTML = numberOfGuesses;
-    
+
     // currentWord = getRandomWord();
     // for(var i=0; i<currentWord.length;i++){
     //     currentWordDashes.push(" _ " + "  ");
     // }
     // document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
-     document.onkeyup = function(event) {
-    	// if(currentWordDashes.){
+    document.onkeyup = function(event) {
+        // if(currentWordDashes.){
 
-    	// }
-    	console.log(currentWord);
-    	decideWinner();
-    	letterGuessed = event.key;
-    	//console.log(lettersGuessedAndCorrect.length + "--" + currentWord.length);
-    	if(lettersGuessedAndCorrect.length === currentWord.length){
-           resetTheGame();
-           currentWord = getRandomWord();
-           for(var i=0; i<currentWord.length;i++){
-           		currentWordDashes.push(" _ " + "  ");
-           }
-           document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
-        }
-        
-
-        if(!checkTheLetterGuessedIfAlreadyGuessed(lettersAlreadyGuessed,letterGuessed)){
-            lettersAlreadyGuessed.push(letterGuessed);
-            guessesRemaining--;
-            document.querySelector("#numberOfGuesses").innerHTML = guessesRemaining;
-        	document.querySelector("#lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed.join(" ");
-        
-            if(checkTheLetterGuessedIfExistsInCurrentWord(currentWord,letterGuessed)){
-            lettersGuessedAndCorrect.push(letterGuessed);
-            updateTheAnswer(currentWord,letterGuessed);
-            document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
-        	
-
-        	}
-        }
-        //decideWinner();
-        if(guessesRemaining === 0){
-        	resetTheGame();
-        }
-        // console.log(currentWordDashes.join(" "));
+        // }
         console.log(currentWord);
         decideWinner();
-        
+        letterGuessed = event.keyCode;
+        if ((letterGuessed >= 65 && letterGuessed <= 90)) {
+            letterGuessed = String.fromCharCode((96 <= letterGuessed && letterGuessed <= 105) ? letterGuessed-48 : letterGuessed).toLowerCase();;
+            //console.log(lettersGuessedAndCorrect.length + "--" + currentWord.length);
+            if (lettersGuessedAndCorrect.length === currentWord.length) {
+                resetTheGame();
+                currentWord = getRandomWord();
+                for (var i = 0; i < currentWord.length; i++) {
+                    currentWordDashes.push(" _ ");
+                }
+                document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
+            }
+
+
+            if (!checkTheLetterGuessedIfAlreadyGuessed(lettersAlreadyGuessed, letterGuessed)) {
+                lettersAlreadyGuessed.push(letterGuessed);
+                guessesRemaining--;
+                document.querySelector("#numberOfGuesses").innerHTML = guessesRemaining;
+                document.querySelector("#lettersAlreadyGuessed").innerHTML = lettersAlreadyGuessed.join(" ");
+
+                if (checkTheLetterGuessedIfExistsInCurrentWord(currentWord, letterGuessed)) {
+                    lettersGuessedAndCorrect.push(letterGuessed);
+                    updateTheAnswer(currentWord, letterGuessed);
+                    document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
+
+
+                }
+            }
+            //decideWinner();
+            if (guessesRemaining < 0) {
+                resetTheGame();
+            }
+            // console.log(currentWordDashes.join(" "));
+            console.log(currentWord);
+            decideWinner();
+        }
     }
 
 
@@ -67,44 +72,47 @@ document.onkeyup = function(event) {
 
 }
 
-function decideWinner(){
-	console.log(currentWordDashes.join("").toString());
-	console.log(currentWord);
-	if((currentWordDashes.join("").toString() === currentWord) && (guessesRemaining >= 0)){
-		wins++;
-		document.querySelector("#wins").innerHTML = wins;
-		//resetTheGame();
-		if(currentWord === "tiger"){
-			var img = document.getElementById("guessImage");
-        	img.src = "assets/images/tiger.jpg"
-		}
-		else if(currentWord === "apple"){
-			var img = document.getElementById("guessImage");
-        	img.src = "assets/images/apple.jpg"
-		}
-		else if(currentWord === "flower"){
-			var img = document.getElementById("guessImage");
-        	img.src = "assets/images/flower.jpg"
-		}
-		else{
-			var img = document.getElementById("guessImage");
-        	img.src = "assets/images/potatoes.jpg"
-		}
-		resetTheGame();
-		
-	}
+function decideWinner() {
+    // console.log(currentWordDashes.join("").toString());
+    // console.log(currentWord);
+    // console.log(lettersAlreadyGuessed);
+    // console.log(lettersGuessedAndCorrect);
+    if ((currentWordDashes.join("").toString() === currentWord) && (guessesRemaining >= 0)) {
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        //resetTheGame();
+        if (currentWord === "tiger") {
+            var img = document.getElementById("guessImage");
+            img.src = "assets/images/tiger.jpg"
+        } else if (currentWord === "apple") {
+            var img = document.getElementById("guessImage");
+            img.src = "assets/images/apple.jpg"
+        } else if (currentWord === "flower") {
+            var img = document.getElementById("guessImage");
+            img.src = "assets/images/flower.jpg"
+        } else {
+            var img = document.getElementById("guessImage");
+            img.src = "assets/images/potatoes.jpg"
+        }
+        resetTheGame();
+
+    }
 }
 
-
-function updateTheAnswer(currentWord,letterGuessed){
-
-	for(var i=0; i<currentWord.length;i++){
-		if(currentWord[i] === letterGuessed)
-			currentWordDashes[i] = currentWord[i];
-	}
+function alphaOnly(event) {
+    var key = event.keyCode;
+    return ((key >= 65 && key <= 90));
 }
 
-function getRandomWord(){
+function updateTheAnswer(currentWord, letterGuessed) {
+
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === letterGuessed)
+            currentWordDashes[i] = currentWord[i];
+    }
+}
+
+function getRandomWord() {
     currentWord = gameWords[Math.floor(Math.random() * gameWords.length)];
     // if(currentWord === "tiger"){
 
@@ -112,7 +120,7 @@ function getRandomWord(){
     return currentWord;
 }
 
-function checkTheLetterGuessedIfAlreadyGuessed(lettersAlreadyGuessed,letterGuessed) {
+function checkTheLetterGuessedIfAlreadyGuessed(lettersAlreadyGuessed, letterGuessed) {
     for (var i = 0; i < lettersAlreadyGuessed.length; i++) {
         if (letterGuessed === lettersAlreadyGuessed[i]) {
             //console.log(i);
@@ -121,9 +129,9 @@ function checkTheLetterGuessedIfAlreadyGuessed(lettersAlreadyGuessed,letterGuess
     }
 }
 
-function checkTheLetterGuessedIfExistsInCurrentWord(currentWord,letterGuessed) {
-    for(var i = 0; i < currentWord.length;i++){
-        if(letterGuessed === currentWord[i]){
+function checkTheLetterGuessedIfExistsInCurrentWord(currentWord, letterGuessed) {
+    for (var i = 0; i < currentWord.length; i++) {
+        if (letterGuessed === currentWord[i]) {
             return true;
         }
 
@@ -143,18 +151,18 @@ function resetTheGame() {
     document.querySelector("#numberOfGuesses").innerHTML = numberOfGuesses;
     document.querySelector("#lettersAlreadyGuessed").innerHTML = "";
     document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
-       document.querySelector("#getStarted").innerHTML = "Game Started";
+    document.querySelector("#getStarted").innerHTML = "Game Started";
     document.querySelector("#numberOfGuesses").innerHTML = numberOfGuesses;
-    
+
     currentWord = getRandomWord();
-    for(var i=0; i<currentWord.length;i++){
-        currentWordDashes.push(" _ " + "  ");
+    for (var i = 0; i < currentWord.length; i++) {
+        currentWordDashes.push(" _ ");
     }
-    document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");	
+    document.querySelector("#currentWord").innerHTML = currentWordDashes.join(" ");
 
 
 }
 
-function getTheIndexOfTheLetter(letterGuessed,currentWord){
-	return currentWord.indexOf(letterGuessed);
+function getTheIndexOfTheLetter(letterGuessed, currentWord) {
+    return currentWord.indexOf(letterGuessed);
 }
